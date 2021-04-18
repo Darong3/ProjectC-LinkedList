396a24aa614b30198734af36225421bf1efcb612
Darong3 <75179692+Darong3@users.noreply.github.com>
2021-04-18 14:47:09 -0700

    Update LinkedFrontBackCappedList.java

--- LinkedFrontBackCappedList.java
+++ LinkedFrontBackCappedList.java
@@ -3,7 +3,34 @@ public class LinkedFrontBackCappedList<T> implements FrontBackCappedListInterfac
 	private Node head, tail;
    
    // YOUR CLASS HERE!
+	
+	@Override
+	public int size() {
+		return numberOfElements;
+	}
 
+	@Override
+	public boolean isEmpty() {
+		boolean result = true;
+		if(head != null){
+			result = false;
+		}
+		return result;
+	}
+
+	@Override
+	public boolean isFull() {
+		boolean result = true;
+		if(isEmpty()){
+			result = false;
+		}
+		else{
+			if(list.length == numberOfElements){
+				result = false;
+			}
+		}
+		return result;
+	}
 	public class Node {
 		public T data; 
 		public Node next; 
